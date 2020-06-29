684234f91de187ac8d19ec7a4698087f96e2bb4b
Antoine <karagan.app@gmail.com>
2020-06-29 13:57:18 +0200

    adding chat and alerts gateways

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
+import { ChatGateway } from './chat/chat.gateway';
+import { AlertsGateway } from './alerts/alerts.gateway';
+import { AlertsController } from './alerts/alerts.controller';
 
 @Module({
   imports: [SharedModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AlertsController],
+  providers: [AppService, ChatGateway, AlertsGateway],
 })
 export class AppModule {}
