261003b87ea8768bcb6a7d902aa7fe94a3400dda
Antoine <karagan.app@gmail.com>
2020-06-29 17:16:16 +0200

    make shared module Global

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
-import { ChatGateway } from './chat/chat.gateway';
-import { AlertsGateway } from './alerts/alerts.gateway';
-import { AlertsController } from './alerts/alerts.controller';
+// import { ChatGateway } from './chat/chat.gateway';
+// import { AlertsGateway } from './alerts/alerts.gateway';
+// import { AlertsController } from './alerts/alerts.controller';
 
 @Module({
   imports: [SharedModule],
-  controllers: [AppController, AlertsController],
-  providers: [AppService, ChatGateway, AlertsGateway],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { RedisModule } from './redis/redis.module';
 import { SocketStateModule } from './socket-state/socket-state.module';
 import { RedisPropagatorModule } from './redis-propagator/redis-propagator.module';
 
+@Global()
 @Module({
   imports: [
     RedisModule,
