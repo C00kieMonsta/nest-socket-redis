4c06270dba873fdea003f6e8f01730921ee75962
Antoine <karagan.app@gmail.com>
2020-06-30 17:50:01 +0200

    adding comment

--- src/shared/redis-propagator/redis-propagator.module.ts
+++ src/shared/redis-propagator/redis-propagator.module.ts
@@ -3,9 +3,17 @@ import { Module } from '@nestjs/common';
 import { RedisPropagatorService } from './redis-propagator.service';
 import { RedisModule } from '../redis/redis.module';
 
+
 /**
- * This module exposes the redis propagator service responsible for
- * dispatching events across all instances of the application.
+ * The key to scale websockets to multiple instances without having sticky sessions, is to
+ * implement a process that can talk to all instances of the app. This redis propagator is meant
+ * to dispatch events across all instances of the application.
+ * 
+ * The redis propagator service will be listening to any incoming Redis events
+ * from other instances and dispatch events to them as well. Three types of events:
+ *  1. Emit to all open connections
+ *  2. Emit to all authenticated users
+ *  3. Emit to specific user
  */
 @Module({
   imports: [RedisModule],
