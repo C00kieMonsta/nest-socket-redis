acbd13536dd3abf5be72c0ce065c5eda83daeed5
Antoine <karagan.app@gmail.com>
2020-07-01 14:08:02 +0200

    minor change in main file

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { AppModule } from './app.module';
 import { SocketStateService } from './shared/socket-state/socket-state.service';
 import { SocketStateAdapter } from './shared/socket-state/socket-state.adapter';
 import { RedisPropagatorService } from './shared/redis-propagator/redis-propagator.service';
+import { SharedModule } from './shared/shared.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  const socketStateService = app.get(SocketStateService);
-  const redisPropagatorService = app.get(RedisPropagatorService);
+  const socketStateService = app.select(SharedModule).get(SocketStateService);
+  const redisPropagatorService = app.select(SharedModule).get(RedisPropagatorService);
   app.useWebSocketAdapter(new SocketStateAdapter(app, socketStateService, redisPropagatorService));
 
   await app.listen(3000, () => {
